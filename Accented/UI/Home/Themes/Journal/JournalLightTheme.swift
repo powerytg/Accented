dc032ecef003a75f504bdb07e7082f22bc67feb4
Tiangong You <tiangong@amazon.com>
2016-06-05 10:49:16 -0700

    Added journal light theme

--- Accented/UI/Home/Themes/Journal/JournalLightTheme.swift
+++ Accented/UI/Home/Themes/Journal/JournalLightTheme.swift
@@ -18,4 +18,16 @@ class JournalLightTheme: JournalDarkTheme {
         return UIBlurEffect(style: .ExtraLight)
     }
 
+    override var pushButtonBorderColor : UIColor {
+        return UIColor(red: 100 / 255.0, green: 100 / 255.0, blue: 100 / 255.0, alpha: 1)
+    }
+    
+    override var pushButtonBackgroundColor : UIColor {
+        return UIColor(red: 255 / 255.0, green: 255 / 255.0, blue: 255 / 255.0, alpha: 0.9)
+    }
+    
+    override var pushButtonTextColor : UIColor {
+        return UIColor(red: 76 / 255.0, green: 76 / 255.0, blue: 76 / 255.0, alpha: 1)
+    }
+
 }
