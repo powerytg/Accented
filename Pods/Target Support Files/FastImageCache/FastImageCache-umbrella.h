78ddf2ed04df968361f06c82eebaad727ecffaa8
Tiangong You <tiangong@amazon.com>
2016-08-25 17:20:55 -0700

    Added FastImageCache

---
+++ Pods/Target Support Files/FastImageCache/FastImageCache-umbrella.h
@@ -0,0 +1,16 @@
+#import <UIKit/UIKit.h>
+
+#import "FastImageCache.h"
+#import "FICEntity.h"
+#import "FICImageCache+FICErrorLogging.h"
+#import "FICImageCache.h"
+#import "FICImageFormat.h"
+#import "FICImageTable.h"
+#import "FICImageTableChunk.h"
+#import "FICImageTableEntry.h"
+#import "FICImports.h"
+#import "FICUtilities.h"
+
+FOUNDATION_EXPORT double FastImageCacheVersionNumber;
+FOUNDATION_EXPORT const unsigned char FastImageCacheVersionString[];
+
